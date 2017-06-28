2216b872019a207d140007834afa3e96bd46d570
bgriessbach <b.griessbach@gmail.com>
2017-06-28 14:15:27 +0100

    Create balanced_brackets.py

---
+++ balanced_brackets.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        mydict={"(":")", "[":"]", "{":"}"}
+        mylist=[]
+        for item in s:
+            if item in mydict.keys():
+                mylist.append(mydict[item])
+            else:
+                if len(mylist)<1:
+                    return False
+                if mylist.pop()!=item:
+                    return False
+        if len(mylist)!=0:
+            return False
+        return True
