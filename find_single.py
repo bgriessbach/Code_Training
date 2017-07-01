37a79d221c5699323dccafc19d76a975290f5c6f
bgriessbach <b.griessbach@gmail.com>
2017-07-01 19:40:41 +0100

    Update find_single.py

--- find_single.py
+++ find_single.py
@@ -9,3 +9,14 @@ class Solution(object):
         for item in mydict.keys():
             if mydict[item]==1:
                 return item
+//**Second way using set**//
+
+class Solution(object):
+    def singleNumber(self, nums):
+        myset = set()
+        for item in nums:
+            if item not in myset:
+                myset.add(item)
+            else:
+               myset.remove(item)
+        return myset.pop()
