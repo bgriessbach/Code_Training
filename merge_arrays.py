ad289ff9d91c76013ca47db28b7d09cc9fcd4d33
bgriessbach <b.griessbach@gmail.com>
2017-07-16 12:22:11 +0100

    Update merge_arrays.py

--- merge_arrays.py
+++ merge_arrays.py
@@ -4,21 +4,8 @@ class Solution(object):
             nums1.pop()
         while len(nums2)>n:
             nums2.pop()
-        if len(nums1)==0:
-            for item in nums2:
-                nums1.append(item)
-            return
-        start_length=len(nums1)
-        index1=0
-        index2=0
-        while len(nums1)!=len(nums2)+start_length:
-            if nums1[index1]>=nums2[index2]:
-                nums1.insert(index1 ,nums2[index2])
-                index2+=1
-            else:
-                if len(nums1)>=index1+2:
-                    index1+=1
-                else:
-                    nums1.append(nums2[index2])
-                    index2+=1
+        for item in nums2:
+            nums1.append(item)
+        nums1.sort()
+        return
                 
